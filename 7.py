1d96f418b396f81cc7f020bfe4913a1add8090d7
d1zex <ospanaren@mail.ru>
2025-02-28 10:22:32 +0500

    fixed

--- 7.py
+++ 7.py
@@ -2,4 +2,4 @@ def snake_to_camel(word):
         import re
         return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
-print(snake_to_camel('arsen_loh'))
+print(snake_to_camel('ramil_bot'))
